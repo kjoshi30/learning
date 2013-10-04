6863d7215d8af8f082fd04e13fad3b89d9875aff
kaustubh joshi <thekaustubhjosih@gmail.com>
2013-10-04 12:05:38 +0530

    shape class ready

--- volumes.java
+++ volumes.java
@@ -1,1 +1,16 @@
 class shape{
+int length,breadth,height;
+	public shape(int a,int b,int c){
+	length = a;
+	breadth = b;
+	height = c;
+
+	}
+	
+	public int volume(){
+	return a*b*c;
+	}
+	
+	}
+	
+	
