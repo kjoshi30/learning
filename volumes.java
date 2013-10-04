c967bd075f98df78ac5b2a6623aec90b6b1bdf6e
kaustubh joshi <thekaustubhjosih@gmail.com>
2013-10-04 12:18:12 +0530

    cube class complete

--- volumes.java
+++ volumes.java
@@ -1,16 +1,29 @@
+import java.util.Scanner;
 class shape{
-int length,breadth,height;
+	int length,breadth,height;
 	public shape(int a,int b,int c){
-	length = a;
-	breadth = b;
-	height = c;
-
+		length 	= a;
+		breadth = b;
+		height 	= c;
 	}
 	
 	public int volume(){
-	return a*b*c;
+		return a*b*c;
 	}
+}
 	
+class cube{
+	public static void main(String args[]){
+		/*
+			extend shape class and caluclate volume of a cube
+		*/
+		System.out.println("ENter dimensions:");
+		Scanner x = new Scanner(System.in);
+		int c = x.nextInt();
+		int d = x.nextInt();
+		int f = x.nextInt();
+		
+		shape cube = new shape(c,d,f);
+		System.out.printl(cube.volume());
 	}
-	
-	
+}
